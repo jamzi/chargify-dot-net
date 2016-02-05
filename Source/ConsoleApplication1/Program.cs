801313ee424471e580b209e1da103a9d77dd23e6
Kori Francis <byter@me.com>
2016-02-05 10:14:33 -0500

    wip

---
+++ Source/ConsoleApplication1/Program.cs
@@ -0,0 +1,32 @@
+﻿using ChargifyNET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var Chargify = new ChargifyConnect("https://subdomain.chargify.com/", "api-key", "X");
+
+                // Do some work here
+
+                Console.WriteLine("Done!");
+            }
+            catch (ChargifyException chEx)
+            {
+                Console.WriteLine(chEx.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
